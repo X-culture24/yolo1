500679b4360fd365cb7dce7478d6fa0be6d018b6
lawrence <lawrenceimani42@gmail.com>
2025-01-08 10:22:47 +0300

    Added README and Terraform configurations

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,6 +3,12 @@ provider "aws" {
   access_key = "AKIAQJZCDRQIZRE55ZEK"
   secret_key = "T4qehzGcUPB/3Kzf12f0St4ir/RutrER8F15Odwp"
 }
+resource "aws_instance" "example" {
+  ami           = "ami-0ac80df6eff0e70b5" # Replace with a valid AMI ID
+  instance_type = "t2.micro"
 
-
+  tags = {
+    Name = "ExampleInstance"
+  }
   
+}
